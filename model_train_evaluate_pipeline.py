5517e6d4af6b61efd4b74a96ec8eaa0848573ca7
prashanth99 <prashanthrajaiah99@gmail.com>
2019-06-25 18:22:11 +0530

    Solved the dependency bug

--- model_train_evaluate_pipeline.py
+++ model_train_evaluate_pipeline.py
@@ -1,5 +1,4 @@
 # Deep learning model training and inference pipeline
-
 import luigi
 import pandas as pd
 
@@ -14,6 +13,27 @@ class DownloadDataset(luigi.Task):
 
     data_path = luigi.Parameter()
 
+    def output(self):
+        return luigi.LocalTarget('./base_dataframe.csv')
+
     def run(self):
         house_price_data = pd.read_csv(self.data_path)
-        print(house_price_data)
+        house_price_data.to_csv(self.output().path)
+        return house_price_data
+
+"""
+This task/module will be handling the Preprocessing tasks that are necessary to perform before building the
+the architecture of the model
+
+The major difference in the DataPreprocessing Task will be that it will be overiding the requires() and will
+obtain the necessary information from DownloadDataset class.
+"""
+
+class DataPreprocessing(luigi.Task):
+
+    data_path_extract = luigi.Parameter()
+
+    def requires(self):
+        return DownloadDataset(self.data_path_extract)
+
+    
