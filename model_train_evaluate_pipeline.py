7bbf7c35660b1e34c3c2176eb1496eb143dcc790
prashanth99 <prashanthrajaiah99@gmail.com>
2019-06-26 12:48:13 +0530

    Completion of Data Preprocessing Module

--- model_train_evaluate_pipeline.py
+++ model_train_evaluate_pipeline.py
@@ -1,6 +1,9 @@
 # Deep learning model training and inference pipeline
 import luigi
 import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 
 """
@@ -36,4 +39,48 @@ class DataPreprocessing(luigi.Task):
     def requires(self):
         return DownloadDataset(self.data_path_extract)
 
-    
+    def output(self):
+        #return luigi.LocalTarget(['./X_train.npy','X_val.npy'])
+        return {'X_train_path':luigi.LocalTarget('./X_train.npy'),
+        'X_val_path':luigi.LocalTarget('./X_val.npy'),
+        'X_test_path':luigi.LocalTarget('./X_test.npy'),
+        'Y_train_path':luigi.LocalTarget('./Y_train.npy'),
+        'Y_val_path':luigi.LocalTarget('./Y_val.npy'),
+        'Y_test_path':luigi.LocalTarget('./Y_test.npy')}
+
+
+    def run(self):
+        input_path = self.input().path
+        base_dataframe = pd.read_csv(input_path)
+        # Converting the dataframe into an array.
+        dataset = base_dataframe.values
+        X = dataset[:,0:10]
+        Y = dataset[:,10]
+
+        min_max_scaler = preprocessing.MinMaxScaler()
+        X_scale = min_max_scaler.fit_transform(X)
+
+        # Using sklearn test_train_split to split data into test and train dataset
+        X_train, X_val_and_test, Y_train, Y_val_and_test = train_test_split(X_scale, Y, test_size=0.3)
+
+        # Usinf train_test_split to create validation data
+        X_val, X_test, Y_val, Y_test = train_test_split(X_val_and_test, Y_val_and_test, test_size=0.5)
+
+        # Printing the shape of all the variables
+        print(X_train.shape, X_val.shape, X_test.shape, Y_train.shape, Y_val.shape, Y_test.shape)
+
+        # Saving the numpy arrays to their designated location which will be utilized while model creation
+        np.save('./X_train.npy', X_train)
+        np.save('./X_val.npy', X_val)
+        np.save('./X_test.npy', X_test)
+        np.save('./Y_train.npy', Y_train)
+        np.save('./Y_val.npy', Y_val)
+        np.save('./Y_test.npy', Y_test)
+
+
+"""
+This Module/Task is responsible for building model architecture.
+"""
+
+class ModelBuilding(luigi.Task):
+    pass
